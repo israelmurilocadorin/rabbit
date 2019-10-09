9395677b7da1ef84713bd6704c9534768b7b76e3
israelmurilocadorin <israel.cadorin@catolicasc.org.br>
2019-10-09 16:23:25 -0300

    Mensagem

--- enviar.py
+++ enviar.py
@@ -5,10 +5,10 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost')
 channel = connection.channel()
 
 #Criação da fila que recebe a mensagem
-channel.queue_declare(queue = 'fila')
+channel.queue_declare(queue = 'ola')
 
 #Publica a mensagem na fila especificada
-channel.basic_publish(exchange='', routing_key='fila', body='Hello World!')
-print("Hello World!")
+channel.basic_publish(exchange='', routing_key='ola', body='Hello World!')
+print("[x] Enviado 'Hello World!'")
 connection.close()
 
--- recebe.py
+++ recebe.py
@@ -9,11 +9,11 @@ channel.queue_declare(queue='ola')
 
 #Função que imprime na tela a mensagem
 def ​callback(ch, method, properties, body):
-   ​print(" [x] Received %r" % body)
+   ​print(" [x] Recebido %r" % body)
 
 #Informa o RabbitMQ que essa função deve receber mensagens só da fila ola
 channel.basic_consume(queue='ola', on_message_callback=callback, auto_ack=True)
 
 #Loop que executa os retornos sempre que necessário
-print(' [*] Waiting for messages. To exit press CTRL+C')
+print(' [*] Aguardando mensagens. Para sair pressione CTRL+C')
 channel.start_consuming()
